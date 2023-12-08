98880b751eaa2f42052b1448a59d91bdc16b2417
diakhoumpa <diakhoumpam.com>
2023-12-08 13:32:07 +0000

    ajout methode dans DepotServices

--- src/main/java/com/groupeisi/quality/service/DepotService.java
+++ src/main/java/com/groupeisi/quality/service/DepotService.java
@@ -1,4 +1,11 @@
 package com.groupeisi.quality.service;
 
 public class DepotService {
+    public String message(){
+
+        return "hello la classe nous avons cours tous les mercredi et vendredi";
+    }public String welcome(){
+
+        return "Bienvenu en classe de ngor seck";
+    }
 }
